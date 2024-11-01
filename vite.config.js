4c0311d7fb4b0f2a2d07ec754d5957731a3cf7af
TatsuyaMaeta <tat.mae084@gmail.com>
2024-11-01 16:48:35 +0900

    change

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+const github = import.meta.env.VITE_GITHUB_PAGES;
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: process.env.GITHUB_PAGES // この行を追加
+    base: github // この行を追加
         ? "reactailwind" // この行を追加
         : "./", // この行を追加
-        plugins: [react()],
+    plugins: [react()],
+    define: {
+        "process.env": {},
+    },
 });
